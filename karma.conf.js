3d04a4454cf73750f4222be774cbd0e6fe6a2b0c
pyaephyo-lab <kopyaephyoaung1994@gmail.com>
2026-01-13 17:03:01 +0000

    Config change karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,13 @@
-// Karma configuration file, see link for more information
+// Karma configuration file
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+
+process.env.CHROME_BIN = process.env.CHROME_BIN || '/usr/bin/google-chrome';
 
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -13,18 +16,16 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false
     },
+
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/toh'),
       subdir: '.',
@@ -33,13 +34,29 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+
+    reporters: ['progress', 'junit'],
+
     junitReporter: {
       outputDir: 'test-results',
       outputFile: 'test-results.xml',
       useBrowserName: false
     },
-    browsers: ['Chrome'],
-    restartOnFileChange: true
+
+    // ✅ IMPORTANT PART
+    browsers: ['ChromeHeadlessCI'],
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    },
+
+    restartOnFileChange: false
   });
 };
